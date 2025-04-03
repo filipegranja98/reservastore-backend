f7ad683062593bbf4f60ebc80925f35231f28444
Filipe da Silva Granja <filipe.00000847940@unicap.br>
2025-04-03 15:38:10 -0300

    Corrigindo Procfile para deploy no Railway

--- reservastoresupa/core/settings.py
+++ reservastoresupa/core/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kb%4*7d5$jb18$-*&eqzl#rtf&s)#begwe=h2))0q5t&w=of&!'
+
 # Application definition
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -98,6 +99,7 @@ DATABASES = {
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
         'PORT': os.getenv('PORT'),
+
     }
 }
 
