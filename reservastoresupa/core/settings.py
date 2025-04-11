a52ba46edde4d75c545bdd16fb61084c7b627169
Filipe da Silva Granja <filipe.00000847940@unicap.br>
2025-04-11 17:48:26 -0300

    customized commit message

--- reservastoresupa/core/settings.py
+++ reservastoresupa/core/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.lojas',
+    'reservastoresupa.lojas',
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
